a251d7b40e9664675a74fc7fca20db32754b6c32
Alejandro <rb.jandro@gmail.com>
2025-06-02 16:34:10 +0200

    Rename infrastructure to infra

---
+++ infra/postgres/init.sql
@@ -0,0 +1,14 @@
+CREATE SCHEMA IF NOT EXISTS mlflow;
+CREATE SCHEMA IF NOT EXISTS predictions;
+
+-- CREATE TABLE IF NOT EXISTS predictions.prediction_logs (
+--     id SERIAL PRIMARY KEY,
+--     timestamp TIMESTAMPTZ DEFAULT NOW(),
+--     input_data JSONB NOT NULL,
+--     prediction_result JSONB NOT NULL,
+--     model_name TEXT NOT NULL,
+--     model_version TEXT,
+--     mlflow_run_id TEXT,
+--     user_notes TEXT,
+--     extra_metadata JSONB
+-- );
