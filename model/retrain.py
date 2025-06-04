b01d19894cb1146b25d7fea840e9cc5196d7fa09
Alejandro <rb.jandro@gmail.com>
2025-06-04 12:32:28 +0200

    Update model/retrain.py

--- model/retrain.py
+++ model/retrain.py
@@ -1,22 +1,25 @@
 import mlflow
+from db.client import db
+from db.models import PredictionFeedback
 from mlflow_utils.config import configure_mlflow
 from mlflow_utils.model_io import (
     load_latest_model,
     log_model,
     register_model,
 )
-from model.utils import load_data, eval_model, load_config, create_pipeline_from_config
-from db.client import db
-from db.models import PredictionFeedback
-from utils.settings import MODEL_CONFIG_PATH, MIN_IMPROVEMENT_DELTA
+from model.utils import eval_model, load_data
+from utils.settings import MIN_IMPROVEMENT_DELTA
 
 
 def retrain_if_enough_feedback(min_feedbacks: int = 10):
+    print("Retraining if enough feedback")
+    print("Configuring MLflow")
     configure_mlflow()
 
-    config = load_config(MODEL_CONFIG_PATH)
+    print("Loading data")
     X_train, X_test, y_train, y_test = load_data(include_feedback=True)
 
+    print("Loading feedbacks")
     feedbacks = (
         db.query(PredictionFeedback)
         .filter(PredictionFeedback.used_in_retraining_run_id == None)
@@ -26,17 +29,18 @@ def retrain_if_enough_feedback(min_feedbacks: int = 10):
         print(f"Not enough feedbacks ({len(feedbacks)} < {min_feedbacks})")
         return
 
-    current_model = load_latest_model()
-    current_rmse = eval_model(current_model, X_test, y_test)
+    print("Loading latest model")
+    model = load_latest_model()
+    print("Evaluating current model")
+    current_rmse = eval_model(model, X_test, y_test)
 
     with mlflow.start_run() as run:
-        pipeline = create_pipeline_from_config(config)
-        pipeline.fit(X_train, y_train)
-        new_rmse = eval_model(pipeline, X_test, y_test)
+        model.fit(X_train, y_train)
+        new_rmse = eval_model(model, X_test, y_test)
 
         mlflow.log_metric("rmse", float(new_rmse))
         mlflow.log_param("retrain_feedbacks", len(feedbacks))
-        log_model(pipeline)
+        log_model(model)
 
         if current_rmse - new_rmse >= MIN_IMPROVEMENT_DELTA:
             print("Promoting new model to production")
